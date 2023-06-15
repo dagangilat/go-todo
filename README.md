ec93f135640ca34269b920e7904d5b50436b031e
dagangilat <dagan.gilat@gmail.com>
2023-06-15 13:43:01 +0300

    go-todo v1.1

--- README.md
+++ README.md
@@ -1,97 +1,68 @@
-# TODO Application
+# Todo App
 
-This is a simple TODO list application with a Go backend and a React frontend.
+This is a simple Todo app server built with Go. It allows you to manage tasks by providing various API endpoints.
 
-## Deployment
+## API Endpoints
 
-### Backend
-
-1. Navigate to the backend directory.
-
-   ```bash
-   cd /path/to/your/go/project
-   ```
-
-2. Build the Go project.
-
-   ```bash
-   go build -o app
-   ```
-
-3. Run the Go app.
-
-   ```bash
-   ./app
-   ```
-
-### Frontend
-
-1. Navigate to the frontend directory.
-
-   ```bash
-   cd /path/to/your/react/project
-   ```
-
-2. Install dependencies.
-
-   ```bash
-   npm install
-   ```
-
-3. Build the React app.
-
-   ```bash
-   npm run build
-   ```
-
-4. Serve the app on port 5000.
-
-   ```bash
-   npm install -g serve
-   serve -s build -l 5000
-   ```
+- `GET /tasks`: Retrieves all tasks.
+- `POST /tasks`: Adds a new task.
+- `PUT /tasks/{id}`: Updates an existing task.
+- `DELETE /tasks/{id}`: Deletes a task.
+- `GET /tasks/{id}`: Retrieves a single task by its ID.
 
 ## Usage
 
-You can interact with the application through the following endpoints:
+1. Make sure you have a MySQL database set up.
+2. Update the database connection credentials in the code.
+3. Build and run the Go application.
 
-- **Create a task**
+## Configuration
 
-  ```bash
-  curl -X POST -H "Content-Type: application/json" -d '{"text": "Task 1"}' http://localhost:8000/tasks
-  ```
+You can enable/disable debug logging by setting the `DEBUG` variable in the code.
 
-- **List all tasks**
+## Dependencies
 
-  ```bash
-  curl http://localhost:8000/tasks
-  ```
+This application relies on the following dependencies:
 
-- **Get a specific task**
+- `github.com/gorilla/mux`: A powerful URL router and dispatcher for Go.
+- `github.com/go-sql-driver/mysql`: A MySQL driver for Go's database/sql package.
 
-  ```bash
-  curl http://localhost:8000/tasks/{id}
-  ```
+Make sure to install these dependencies using Go's package manager before running the application.
 
-  Replace `{id}` with the ID of the task you want to retrieve.
+## Examples
 
-- **Update a task**
+Here are some examples of how to use the API endpoints with cURL:
 
-  ```bash
-  curl -X PUT -H "Content-Type: application/json" -d '{"text": "Updated Task", "completed": true}' http://localhost:8000/tasks/{id}
-  ```
+### Get all tasks
 
-  Replace `{id}` with the ID of the task you want to update.
-
-- **Delete a task**
-
-  ```bash
-  curl -X DELETE http://localhost:8000/tasks/{id}
-  ```
-
-  Replace `{id}` with the ID of the task you want to delete.
+```bash
+curl -X GET http://localhost:8000/tasks
 ```
 
-Replace `/path/to/your/go/project` and `/path/to/your/react/project` with the paths to your Go and React projects respectively. Also, update `localhost:8000` and `localhost:5000` to match your server's address and port if they're different.
+### Add a new task
 
-This README assumes that your Go server is running on port 8000 and your React app is running on port 5000. If your ports are different, you should update the `curl` commands to reflect your actual configuration.
+```bash
+curl -X POST -H "Content-Type: application/json" -d '{"text":"Task 1", "completed": false}' http://localhost:8000/tasks
+```
+
+### Update an existing task
+
+```bash
+curl -X PUT -H "Content-Type: application/json" -d '{"text":"Updated Task", "completed": true}' http://localhost:8000/tasks/{id}
+```
+
+### Delete a task
+
+```bash
+curl -X DELETE http://localhost:8000/tasks/{id}
+```
+
+### Get a single task by ID
+
+```bash
+curl -X GET http://localhost:8000/tasks/{id}
+```
+
+Replace `{id}` with the actual ID of the task you want to modify or retrieve.
+
+Feel free to customize and modify these cURL examples to suit your needs.
--- main.go
+++ main.go
@@ -19,9 +19,12 @@ type Task struct {
 	Completed bool   `json:"completed"`
 }
 
-var db *sql.DB
+var (
+	db    *sql.DB
+	DEBUG bool = true
+)
 
-// Initialize database connection.
+// initDB initializes the database connection.
 func initDB() {
 	var err error
 	// Establish a connection to the database.
@@ -34,10 +37,21 @@ func initDB() {
 	if err = db.Ping(); err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println("Database connection initialized")
 }
 
-// Fetch all tasks from database.
+// logOperation logs the operation to the console if DEBUG is enabled.
+func logOperation(operation string) {
+	if DEBUG {
+		log.Println(operation)
+	}
+}
+
+// getTasksHandler fetches all tasks from the database.
 func getTasksHandler(w http.ResponseWriter, r *http.Request) {
+	logOperation("List all tasks")
+
 	// Query the database.
 	rows, err := db.Query("SELECT id, text, completed FROM tasks")
 	if err != nil {
@@ -63,8 +77,10 @@ func getTasksHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(tasks)
 }
 
-// Add a new task to the database.
+// addTaskHandler adds a new task to the database.
 func addTaskHandler(w http.ResponseWriter, r *http.Request) {
+	logOperation("Create a new task")
+
 	// Parse the incoming data.
 	var task Task
 	err := json.NewDecoder(r.Body).Decode(&task)
@@ -93,11 +109,13 @@ func addTaskHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(task)
 }
 
-// Edit an existing task.
+// editTaskHandler updates an existing task.
 func editTaskHandler(w http.ResponseWriter, r *http.Request) {
 	// Retrieve the ID from the URL.
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
+
+	logOperation(fmt.Sprintf("Update task with ID %d", id))
 
 	// Parse the incoming data.
 	var task Task
@@ -121,11 +139,13 @@ func editTaskHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(task)
 }
 
-// Delete a task from the database.
+// deleteTaskHandler deletes a task from the database.
 func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
 	// Retrieve the ID from the URL.
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
+
+	logOperation(fmt.Sprintf("Delete task with ID %d", id))
 
 	// Remove the item from the database.
 	_, err := db.Exec("DELETE FROM tasks WHERE id = ?", id)
@@ -136,6 +156,30 @@ func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Return a 204 status to indicate success but with no response body.
 	w.WriteHeader(http.StatusNoContent)
+}
+
+// getTaskByIDHandler retrieves a single task by its ID.
+func getTaskByIDHandler(w http.ResponseWriter, r *http.Request) {
+	// Retrieve the ID from the URL.
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	logOperation(fmt.Sprintf("Get task with ID %d", id))
+
+	// Query the database for the task.
+	row := db.QueryRow("SELECT id, text, completed FROM tasks WHERE id = ?", id)
+
+	// Populate the task struct with the retrieved data.
+	task := &Task{}
+	err := row.Scan(&task.ID, &task.Text, &task.Completed)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Return JSON response.
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
 }
 
 func main() {
@@ -149,9 +193,9 @@ func main() {
 	r.HandleFunc("/tasks", addTaskHandler).Methods("POST")
 	r.HandleFunc("/tasks/{id}", editTaskHandler).Methods("PUT")
 	r.HandleFunc("/tasks/{id}", deleteTaskHandler).Methods("DELETE")
+	r.HandleFunc("/tasks/{id}", getTaskByIDHandler).Methods("GET")
 
-	// Print REDIS server start message to the terminal
-	fmt.Println("Starting Todo app server on port 8001")
-
-	log.Fatal(http.ListenAndServe(":8001", r))
+	// Print server start message to the terminal.
+	fmt.Println("Starting Todo app server on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
